ae1df074f5188226d9b009b396187de746f56806
Mayank-TN <mtandon10@gmail.com>
2022-10-31 16:40:35 +0530

    axios calls

--- main.js
+++ main.js
@@ -1,49 +1,123 @@
 // GET REQUEST
-function getTodos() {
-  console.log('GET Request');
+async function getTodos() {
+  try {
+    let result = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    showOutput(result)
+  }
+  catch (error) {
+    console.log(error)
+  }
 }
 
 // POST REQUEST
-function addTodo() {
-  console.log('POST Request');
+async function addTodo() {
+  let data = {
+    title : 'Shooping',
+    completed : false
+  }
+  let result = await axios.post('https://jsonplaceholder.typicode.com/todos' , data)
+  showOutput(result)
 }
 
 // PUT/PATCH REQUEST
-function updateTodo() {
-  console.log('PUT/PATCH Request');
+async function updateTodo() {
+  let newData = {
+    title : "Update Todo",
+    completed : true
+  }
+  let result = await axios.patch('https://jsonplaceholder.typicode.com/todos/1' , newData)
+  showOutput(result)
 }
 
 // DELETE REQUEST
-function removeTodo() {
-  console.log('DELETE Request');
+async function removeTodo() {
+  let result = await axios.delete('https://jsonplaceholder.typicode.com/todos/200')
+  showOutput(result)
 }
 
 // SIMULTANEOUS DATA
-function getData() {
-  console.log('Simultaneous Request');
+async function getData() {
+  let [todos , posts] = await axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos'),
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+  ])
+
+  console.log(todos)
+  console.log(posts)
+  showOutput(posts)
 }
 
 // CUSTOM HEADERS
-function customHeaders() {
-  console.log('Custom Headers');
+async function customHeaders() {
+  let data = {
+    title : 'Shooping',
+    completed : false
+  }
+  let config = {
+    headers : {
+    "Content-type" : "application/json" ,
+    Authorization : false
+    }
+  }
+  let result = await axios.post('https://jsonplaceholder.typicode.com/todos' , data , config)
+  showOutput(result)
+  
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
-function transformResponse() {
+async function transformResponse() {
+  options = {
+    method : 'post' ,
+    url : 'https://jsonplaceholder.typicode.com/todos' ,
+    data : {
+      title : 'hello world'
+    },
+    transformResponse : axios.defaults.transformResponse.concat(data=>{
+      data.title = data.title.toUpperCase()
+      return data
+    })
+  }
+  let result = await axios(options)
+  showOutput(result)
   console.log('Transform Response');
 }
 
 // ERROR HANDLING
-function errorHandling() {
-  console.log('Error Handling');
+async function errorHandling() {
+  try {
+    let result = await axios.get('https://jsonplaceholder.typicode.com/todoss?_limit=5')
+    showOutput(result)
+  }
+  catch (error) {
+    console.log(error.response.data)
+    console.log(error.response.status)
+  }
 }
 
 // CANCEL TOKEN
-function cancelToken() {
-  console.log('Cancel Token');
+async function cancelToken() {
+  const source = axios.CancelToken.source()
+  if(true){
+    source.cancel('Request Cancel')
+  }
+  try{
+  let result = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5' ,{
+    cancelToken : source.token
+  } )
+    showOutput(result)
+}catch(error){
+  if(axios.isCancel(error)) console.log('Request Cancelled ' + error.message)
+}
+
+
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(config => {
+  console.log(`${config.method.toUpperCase()} is sent to ${config.url} at ${new Date()}`)
+  return config
+}
+)
 
 // AXIOS INSTANCES
 
@@ -90,8 +164,7 @@ document.getElementById('update').addEventListener('click', updateTodo);
 document.getElementById('delete').addEventListener('click', removeTodo);
 document.getElementById('sim').addEventListener('click', getData);
 document.getElementById('headers').addEventListener('click', customHeaders);
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
+document.getElementById('transform').addEventListener('click', transformResponse);
 document.getElementById('error').addEventListener('click', errorHandling);
 document.getElementById('cancel').addEventListener('click', cancelToken);
+
